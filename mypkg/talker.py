32edca4e52470a6f1eee5e2e5859c837dbb05c73
Taisei Suzuki <iseita31kamekichi@gmail.com>
2024-11-21 16:01:06 +0900

    ９回途中経過

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,22 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.srv import Query
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Person,"person",10)
-n = 0
 
 
-def cb():
-    global n
-    msg = Person()
-    msg.name = "鈴木泰生"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
+def cb(request,response):
+    if request.name == "鈴木泰生"
+        response.age = 20
+    else:
+        response.age = 255
+
+
+    return response
 
 
 def main():
-    node.create_timer(0.5,cb)
+    srv = node.create_service(Query, "query", cb)
     rclpy.spin(node)
