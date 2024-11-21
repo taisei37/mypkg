a93c82891af8e5fc80c9f16d95fe1c241218ca2c
Taisei Suzuki <iseita31kamekichi@gmail.com>
2024-11-21 15:26:10 +0900

    11月２１日授業

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'mypkg'
 
@@ -10,18 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Taisei　Suzuki',
+    maintainer='Taisei Suzuki',
     maintainer_email='iseita31kamekichi@gmail.com',
     description='ロボットシステム学のサンプル',
     license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'talker = mypkg.talker:main', #talker.pyのmain関数という意味
-        #'listener = mypkg.listener:main', ←書いておいて後でコメントアウト   
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
         ],
     },
 )
