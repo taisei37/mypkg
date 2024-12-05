4f52f3dd355a9b4f912a083cbcfb284933c950ff
Taisei Suzuki <iseita31kamekichi@gmail.com>
2024-12-05 12:30:36 +0900

    lesson8 once

---
+++ test/test.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+　
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep 'listen: 10'
